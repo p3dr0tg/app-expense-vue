c0c501eb3370c0920dcabad729f3f987b6211d28
p3dr0tg <p3dr0tg@gmail.com>
2020-02-20 00:30:32 -0500

    --Added moving amount class --fix  css progress-bar summary --Remove unnecessary firebase package --Updated package to its current version

---
+++ dist/precache-manifest.b0a15bebf300ec4d6b8e59fb5f83026a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ba127eff0d6879baee",
+    "url": "/css/app.ef0ec21a.css"
+  },
+  {
+    "revision": "d64501a01e7d1f33b0fd",
+    "url": "/css/chunk-vendors.b749ab74.css"
+  },
+  {
+    "revision": "d7e60f9d1433a45ed71817f6d23abeca",
+    "url": "/css/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2"
+  },
+  {
+    "revision": "411a4500de3a4c116f69dd89ecbb3e0e",
+    "url": "/css/font.css"
+  },
+  {
+    "revision": "dea19272b33dadfe3bad6e0e61a6eb20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d13e28dff9b51f427435",
+    "url": "/js/about.d08712f2.js"
+  },
+  {
+    "revision": "b3ba127eff0d6879baee",
+    "url": "/js/app.55d30b5a.js"
+  },
+  {
+    "revision": "d64501a01e7d1f33b0fd",
+    "url": "/js/chunk-vendors.1de2def4.js"
+  },
+  {
+    "revision": "d245d8bb9d92482b37d9f84ba17c7c2c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
